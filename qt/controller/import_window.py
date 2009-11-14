b63acfc738f2c80215808d2d12e34eeb847dc2a8
hsoft <none@none>
2009-11-14 15:32:36 +0000

    qt: added the target account selector and the swap box to the import window.
    --HG--
    extra : convert_revision : svn%3A7afd0f2a-1d33-46b8-9979-f8add37626ee/trunk%40191

--- qt/controller/import_window.py
+++ qt/controller/import_window.py
@@ -8,9 +8,10 @@
 # which should be included with this package. The terms are also available at 
 # http://www.hardcoded.net/licenses/hs_license
 
+from PyQt4.QtCore import Qt
 from PyQt4.QtGui import QWidget
 
-from moneyguru.gui.import_window import ImportWindow as ImportWindowModel
+from moneyguru.gui.import_window import ImportWindow as ImportWindowModel, DAY, MONTH, YEAR
 from .import_table import ImportTable
 from ui.import_window_ui import Ui_ImportWindow
 
@@ -26,18 +27,26 @@ class ImportWindow(QWidget, Ui_ImportWindow):
         
         self.tabView.tabCloseRequested.connect(self.tabCloseRequested)
         self.tabView.currentChanged.connect(self.currentTabChanged)
+        self.targetAccountComboBox.currentIndexChanged.connect(self.targetAccountChanged)
         self.importButton.clicked.connect(self.importClicked)
+        self.swapButton.clicked.connect(self.swapClicked)
     
     def _setupUi(self):
         self.setupUi(self)
         self.tabView.setTabsClosable(True)
         self.tabView.setDrawBase(False)
         self.tabView.setDocumentMode(True)
+        self.tabView.setUsesScrollButtons(True)
     
     def _setupColumns(self):
         h = self.tableView.horizontalHeader()
         h.setHighlightSections(False)
         h.resizeSection(0, 28)
+        
+        # Can't set widget alignment in a layout in the Designer
+        l = self.targetAccountLayout
+        l.setAlignment(self.targetAccountLabel, Qt.AlignTop)
+        l.setAlignment(self.targetAccountComboBox, Qt.AlignTop)
     
     #--- Event Handlers
     def currentTabChanged(self, index):
@@ -46,9 +55,22 @@ class ImportWindow(QWidget, Ui_ImportWindow):
     def importClicked(self):
         self.model.import_selected_pane()
     
+    def swapClicked(self):
+        swapType = self.swapOptionsComboBox.currentIndex()
+        applyToAll = self.applyToAllCheckBox.isChecked()
+        if swapType in (0, 1, 2):
+            first, second = [(DAY, MONTH), (MONTH, YEAR), (DAY, YEAR)][swapType]
+            if self.model.can_switch_date_fields(first, second):
+                self.model.switch_date_fields(first, second, applyToAll)
+        else:
+            self.model.switch_description_payee(applyToAll)
+    
     def tabCloseRequested(self, index):
         self.model.close_pane(index)
         self.tabView.removeTab(index)
+    
+    def targetAccountChanged(self, index):
+        self.model.selected_target_account_index = index
     
     #--- model --> view
     def close(self):
@@ -58,6 +80,12 @@ class ImportWindow(QWidget, Ui_ImportWindow):
         self.tabView.removeTab(self.tabView.currentIndex())
     
     def refresh(self):
+        # We disconnect the combobox because we don't want the clear() call to set the selected 
+        # target index in the model.
+        self.targetAccountComboBox.currentIndexChanged.disconnect(self.targetAccountChanged)
+        self.targetAccountComboBox.clear()
+        self.targetAccountComboBox.addItems(self.model.target_account_names)
+        self.targetAccountComboBox.currentIndexChanged.connect(self.targetAccountChanged)
         while self.tabView.count():
             self.tabView.removeTab(0)
         for pane in self.model.panes:
@@ -67,4 +95,5 @@ class ImportWindow(QWidget, Ui_ImportWindow):
         index = self.model.selected_pane_index
         if index != self.tabView.currentIndex(): # this prevents infinite loops
             self.tabView.setCurrentIndex(index)
+        self.targetAccountComboBox.setCurrentIndex(self.model.selected_target_account_index)
     
