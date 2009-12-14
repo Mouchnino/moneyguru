7bb2cf1cbfc5e2e337e1d5c82cbfa67affcb599f
hsoft <none@none>
2009-12-14 10:50:46 +0000

    qt: Fixed split edition.
    --HG--
    extra : convert_revision : svn%3A7afd0f2a-1d33-46b8-9979-f8add37626ee/trunk%40345

--- qt/controller/split_table.py
+++ qt/controller/split_table.py
@@ -8,16 +8,18 @@
 # which should be included with this package. The terms are also available at 
 # http://www.hardcoded.net/licenses/hs_license
 
+from PyQt4.QtCore import Qt
+
 from moneyguru.gui.split_table import SplitTable as SplitTableModel
-from .column import Column
+from .column import Column, ACCOUNT_EDIT
 from .table import Table
 
 class SplitTable(Table):
     COLUMNS = [
-        Column('account', 'Account', 88),
+        Column('account', 'Account', 88, editor=ACCOUNT_EDIT),
         Column('memo', 'Memo', 70),
-        Column('debit', 'Debit', 70),
-        Column('credit', 'Credit', 70),
+        Column('debit', 'Debit', 70, alignment=Qt.AlignRight),
+        Column('credit', 'Credit', 70, alignment=Qt.AlignRight),
     ]
     
     def __init__(self, transactionPanel, view):
