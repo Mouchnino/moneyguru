b8c85a05f5294142e74db778c6164b81bcecf034
hsoft <none@none>
2009-12-19 10:14:33 +0000

    cocoa: Adapted to help "packagifying".
    --HG--
    extra : convert_revision : svn%3A7afd0f2a-1d33-46b8-9979-f8add37626ee/trunk%40351

--- cocoa/gen.py
+++ cocoa/gen.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
+# Copyright 2009 Hardcoded Software (http://www.hardcoded.net)
+# 
+# This software is licensed under the "HS" License as described in the "LICENSE" file, 
+# which should be included with this package. The terms are also available at 
+# http://www.hardcoded.net/licenses/hs_license
 
+import sys
+sys.path.insert(0, 'py') # for hsutil and hsdocgen
 import os
 
-os.chdir('help')
-os.system('python -u gen.py')
-os.system('/Developer/Applications/Utilities/Help\\ Indexer.app/Contents/MacOS/Help\\ Indexer moneyguru_help')
-os.chdir('..')
+from help import gen
+
+gen.generate()
+os.system('/Developer/Applications/Utilities/Help\\ Indexer.app/Contents/MacOS/Help\\ Indexer help/moneyguru_help')
 
 os.chdir('py')
 os.system('python -u gen.py')
