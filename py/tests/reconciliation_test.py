81f5e86b862090d7ef6bddd2227fd0ec13122787
hsoft <none@none>
2009-12-26 15:42:02 +0000

    core: further simplification and re-factoring in `reconciliation_test`. I also removed 3 test that have long been irrelevant (concepts from before the 1.0 release).
    --HG--
    extra : convert_revision : svn%3A7afd0f2a-1d33-46b8-9979-f8add37626ee/trunk%40392

--- py/tests/reconciliation_test.py
+++ py/tests/reconciliation_test.py
@@ -7,7 +7,7 @@
 # which should be included with this package. The terms are also available at 
 # http://www.hardcoded.net/licenses/hs_license
 
-from datetime import date, timedelta
+from nose.tools import eq_
 
 from .base import TestCase, TestSaveLoadMixin, CommonSetup as CommonSetupBase
 from ..model.account import LIABILITY
@@ -34,12 +34,12 @@ class Pristine(TestCase):
     
     def test_reconciliation_mode(self):
         #Toggling reconciliation mode on and off
-        self.assertFalse(self.document.in_reconciliation_mode())
+        assert not self.document.in_reconciliation_mode()
         self.document.toggle_reconciliation_mode()
         self.check_gui_calls(self.mainwindow_gui, ['refresh_reconciliation_button'])
-        self.assertTrue(self.document.in_reconciliation_mode())
+        assert self.document.in_reconciliation_mode()
         self.document.toggle_reconciliation_mode()
-        self.assertFalse(self.document.in_reconciliation_mode())
+        assert not self.document.in_reconciliation_mode()
     
 
 class OneEntry(TestCase, CommonSetup):
@@ -50,8 +50,8 @@ class OneEntry(TestCase, CommonSetup):
     def test_toggle_entries_reconciled(self):
         # When reconciliation mode is off, doesn't do anything.
         self.etable.toggle_reconciled()
-        self.assertFalse(self.etable[0].reconciliation_pending)
-        self.assertFalse(self.etable[0].reconciled)
+        assert not self.etable[0].reconciliation_pending
+        assert not self.etable[0].reconciled
     
 
 class OneEntryWithAmountInReconciliationMode(TestCase, CommonSetup):
@@ -62,13 +62,14 @@ class OneEntryWithAmountInReconciliationMode(TestCase, CommonSetup):
         self.document.toggle_reconciliation_mode()
     
     def test_can_reconcile_entry(self):
-        """An entry today is reconciliable"""
-        self.assertTrue(self.etable[0].can_reconcile())
+        # An entry today is reconciliable.
+        assert self.etable[0].can_reconcile()
     
-    def test_can_reconcile_previous_entry(self):
-        """It's not possible to reconcile a previous entry"""
+    def test_can_reconcile_previous_balance_entry(self):
+        # It's not possible to reconcile a previous balance entry.
         self.document.select_next_date_range()
-        self.assertFalse(self.etable[0].can_reconcile())
+        # The first entry is not a "Previous Balance" entry
+        assert not self.etable[0].can_reconcile()
     
     def test_reconcile(self):
         """The entry's reconciled state can be written and read"""
@@ -106,58 +107,44 @@ class OneEntryWithAmountInReconciliationMode(TestCase, CommonSetup):
 
 class OneEntryInTheFuture(TestCase):
     def setUp(self):
+        self.mock_today(2009, 12, 26)
         self.create_instances()
-        self.add_account_legacy()
-        str_date = (date.today() + timedelta(days=1)).strftime('%d/%m/%Y')
-        self.add_entry(str_date, increase='42')
+        self.add_account()
+        self.document.show_selected_account()
+        self.add_entry('27/12/2009', increase='42')
         self.document.toggle_reconciliation_mode()
     
     def test_can_reconcile_entry(self):
-        """It's not possible to reconcile an entry in the future"""
-        self.assertFalse(self.etable[0].can_reconcile())
+        # It's not possible to reconcile an entry in the future.
+        assert not self.etable[0].can_reconcile()
     
     def test_can_set_entry_balance(self):
-        """It's not possible to set an entry's reconciliation balance"""
+        # It's not possible to set an entry's reconciliation balance.
         assert not self.etable.can_edit_cell('balance', 0)
     
 
 class OneEntryInLiability(TestCase):
     def setUp(self):
         self.create_instances()
-        self.add_account_legacy(account_type=LIABILITY)
+        self.add_account(account_type=LIABILITY)
+        self.document.show_selected_account()
         self.add_entry(increase='42')
         self.document.toggle_reconciliation_mode()
     
     def test_entry_balance(self):
-        """The balance of the entry is None"""
+        # The balance of the entry is empty.
         # Previously, it would crash because it would try to negate None
-        self.assertEqual(self.etable[0].balance, '')
+        eq_(self.etable[0].balance, '')
     
 
 class TwoEntries(TestCase):
     def setUp(self):
         self.create_instances()
-        self.add_account_legacy()
+        self.add_account()
+        self.document.show_selected_account()
         self.add_entry(increase='42')
         self.add_entry(increase='21')
         self.document.toggle_reconciliation_mode()
-    
-    def test_reconcile_not_selected(self):
-        """Reconciling also selects the entry."""
-        self.assertEqual(self.etable.selected_index, 1)
-        self.etable[0].toggle_reconciled()
-        self.assertEqual(self.etable.selected_index, 0)
-
-    def test_reconcile_second_before_first(self):
-        """reconcile_balance is set in stone when an entry is reconciled, meaning that the reconcile
-        balance for the 2nd entry will stay at 21 even when the 1st entry is reconciled
-        """
-        self.etable.select([1])
-        row = self.etable.selected_row
-        row.toggle_reconciled()
-        self.etable.select([0])
-        row = self.etable.selected_row
-        row.toggle_reconciled()
     
 
 class ThreeEntries(TestCase, CommonSetup):
@@ -165,18 +152,17 @@ class ThreeEntries(TestCase, CommonSetup):
         self.create_instances()
         self.setup_three_entries_reconciliation_mode()
     
+    def test_reconcile_not_selected(self):
+        # Reconciling also selects the entry.
+        # current selection is the last entry.
+        self.etable[0].toggle_reconciled()
+        eq_(self.etable.selected_index, 0)
+    
     def test_toggle_reconcile_then_save(self):
         # saving the file commits reconciliation
         self.etable[1].toggle_reconciled()
         self.save_file()
-        self.assertTrue(self.etable[1].reconciled)
-    
-    def test_toggle_reconciliation_with_selection_disordered(self):
-        """When toggling reconciliation with mutiple selection, make sure that the order in which
-        the entries are toggled doesn't cause adjustments"""
-        self.etable.select([2, 1, 0])
-        self.etable.toggle_reconciled()
-        self.assertEqual(len(self.etable), 3) # no adjustment created
+        assert self.etable[1].reconciled
     
 
 class ThreeEntriesOneReconciled(TestCase, CommonSetup, TestSaveLoadMixin):
