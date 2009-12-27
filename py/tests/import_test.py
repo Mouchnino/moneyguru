b0ef09906842f7701142f0b557b66769ee379765
hsoft <none@none>
2009-12-27 09:13:55 +0000

    core: Extracted load-related tests from `import_test` and created a new `load_test` unit from them.
    --HG--
    extra : convert_revision : svn%3A7afd0f2a-1d33-46b8-9979-f8add37626ee/trunk%40399

--- py/tests/import_test.py
+++ py/tests/import_test.py
@@ -14,12 +14,12 @@ from nose.tools import eq_
 
 from hsutil.currency import PLN, CAD
 
-from .base import ApplicationGUI, TestCase, TestSaveLoadMixin, TestQIFExportImportMixin
+from .base import ApplicationGUI, TestCase as TestCaseBase
 from ..app import Application
 from ..exception import FileFormatError
 from ..model.date import MonthRange, YearRange
 
-class TestCase(TestCase):
+class TestCase(TestCaseBase):
     def importall(self, filename):
         self.document.parse_file_for_import(filename)
         while self.iwin.panes:
@@ -67,23 +67,6 @@ class Pristine(TestCase):
         self.assertRaises(FileFormatError, self.document.parse_file_for_import, filename)
         self.logged.seek(0)
         self.assertTrue(self.logged.read()) # something (a warning) was logged
-    
-
-class LoadPayeeDescription(TestCase):
-    def setUp(self):
-        self.create_instances()
-        self.document.date_range = MonthRange(date(2008, 3, 1))
-        self.document.load_from_xml(self.filepath('moneyguru', 'payee_description.moneyguru'))
-        self.mainwindow.select_transaction_table()
-    
-    def test_attributes(self):
-        """description and payee attributes load correctly, even when some of them are missing"""
-        self.assertEqual(self.ttable[0].description, 'description')
-        self.assertEqual(self.ttable[0].payee, 'payee')
-        self.assertEqual(self.ttable[1].description, 'description')
-        self.assertEqual(self.ttable[1].payee, '')
-        self.assertEqual(self.ttable[2].description, '')
-        self.assertEqual(self.ttable[2].payee, 'payee')
     
 
 class QIFImport(TestCase):
@@ -296,110 +279,6 @@ class ImportAccountInGroup(TestCase):
         eq_(self.bsheet.assets[0].name, 'Some Asset')
     
 
-class LoadFile(TestCase):
-    # Loads 'simple.moneyguru', a file with 2 accounts and 2 entries in each. Select the first entry.
-    def setUp(self):
-        self.mock_today(2008, 2, 20) # so that the entries are shown
-        self.create_instances()
-        self.add_account() # This is to set the modified flag to true so we can make sure it has been put back to false
-        self.document.load_from_xml(self.filepath('moneyguru', 'simple.moneyguru'))
-        self.mainwindow.select_balance_sheet()
-        self.bsheet.selected = self.bsheet.assets[0]
-        self.bsheet.show_selected_account()
-    
-    def test_add_entry(self):
-        """Adding an entry sets the modified flag"""
-        self.add_entry()
-        self.assertTrue(self.document.is_dirty())
-    
-    def test_change_account_currency(self):
-        """Changing an account currency sets the modified flag"""
-        self.apanel.load()
-        self.apanel.currency = PLN
-        self.apanel.save()
-        self.assertTrue(self.document.is_dirty())
-    
-    def test_delete_account(self):
-        """Removing an account sets the modified flag"""
-        self.mainwindow.select_balance_sheet()
-        self.bsheet.selected = self.bsheet.assets[0]
-        self.bsheet.delete()
-        self.arpanel.ok() # continue deletion
-        self.assertTrue(self.document.is_dirty())
-    
-    def test_delete_entries(self):
-        """Deleting an entry sets the modified flag"""
-        self.etable.delete()
-        self.assertTrue(self.document.is_dirty())
-    
-    def test_delete_transactions(self):
-        """Deleting a transaction sets the modified flag"""
-        self.mainwindow.select_transaction_table()
-        self.ttable.select([0]) # will be automatic at some point
-        self.ttable.delete()
-        self.assertTrue(self.document.is_dirty())
-    
-    def test_change_split(self):
-        """Changing a split in the tpanel doesn't make the app dirty unless it's saved"""
-        self.etable.select([1])
-        self.tpanel.load()
-        self.stable.delete()
-        self.assertFalse(self.document.is_dirty())
-
-    def test_edit_entry(self):
-        # When about to save the document, if an entry is in edition, the document saves the edits first
-        row = self.etable.selected_row
-        row.description = 'foo'
-        self.document.stop_edition()
-        self.assertTrue(self.document.is_dirty()) # We started editing, the flag is on
-        self.etable.save_edits()
-        self.assertTrue(self.document.is_dirty())
-    
-    def test_initial_selection(self):
-        """Right after load, the last entry is selected"""
-        self.assertEqual(self.etable.selected_indexes, [1])
-    
-    def test_not_modified(self):
-        """Loading a file resets the modified flag"""
-        self.assertFalse(self.document.is_dirty())
-    
-    def test_rename_account(self):
-        """Renaming an account sets the modified flag"""
-        self.mainwindow.select_balance_sheet()
-        self.bsheet.selected = self.bsheet.assets[0]
-        self.bsheet.selected.name = 'some other name'
-        self.bsheet.save_edits()
-        self.assertTrue(self.document.is_dirty())
-    
-    def test_save_edit_without_edit(self):
-        """Calling save_entry() without having made changes doesn't set the modified flag"""
-        self.etable.save_edits()
-        self.assertFalse(self.document.is_dirty())
-    
-
-class LoadMultiCurrency(TestCase):
-    # Loads 'multi_currency.moneyguru', a file with 2 accounts and a multi-currency transaction.
-    def setUp(self):
-        self.create_instances()
-        self.document.date_range = MonthRange(date(2008, 2, 1))
-        self.document.load_from_xml(self.filepath('moneyguru', 'multi_currency.moneyguru'))
-        self.mainwindow.select_balance_sheet()
-        self.bsheet.selected = self.bsheet.assets[0]
-        self.bsheet.show_selected_account()
-        self.etable.select([0])
-
-    def test_amounts(self):
-        """The amounts are correctly loaded and the logical imbalance has been dealt with"""
-        self.assertEqual(self.etable[0].increase, '200.00')
-        self.tpanel.load()
-        self.assertEqual(len(self.stable), 4)
-        self.assertEqual(self.stable[0].debit, '200.00')
-        self.assertEqual(self.stable[1].debit, 'PLN 123.45')
-        expected = set(['200.00', 'PLN 123.45'])
-        self.assertTrue(self.stable[2].credit in expected)
-        self.assertTrue(self.stable[3].credit in expected)
-    
-
 class TwoEntriesInRangeSaveThenLoad(TestCase):
     """Two entries having the same date, in range. The app saves to a file then loads the same file.
     """
@@ -422,45 +301,6 @@ class TwoEntriesInRangeSaveThenLoad(TestCase):
         row.increase = '42'
         self.etable.save_edits()
         self.assertEqual(self.etable[0].description, 'first')
-    
-class TwoAccountTwoEntriesInEachWithNonAsciiStrings(TestCase, TestSaveLoadMixin, TestQIFExportImportMixin):
-    """Two accounts, two entries in each. Descriptions, categories and account names contain
-    non-latin characters (a polish 'l'). currencies are set to non-default values. first account
-    is selected.
-    
-    Mixin with TestSaveLoadMixin to make sure all those values come back up alright
-    Same for TestQIFExportImportMixin
-    """
-    def setUp(self):
-        self.create_instances()
-        self.add_account_legacy(u'first_account\u0142', currency=PLN)
-        self.add_entry('3/10/2007', u'first\u0142', transfer='other account', increase='1 usd')
-        self.add_entry('4/10/2007', u'second\u0142', increase='2 usd') # Imbalance
-        self.add_account_legacy(u'second_account\u0142', currency=CAD)
-        self.add_entry('5/10/2007', u'third\u0142', transfer=u'first_account\u0142', decrease='1 usd')
-        self.add_entry('6/10/2007', u'fourth\u0142', transfer='yet another account', decrease='2 usd')
-    
-
-class LoadTwice(TestCase, TestSaveLoadMixin):
-    # Loads 'simple.moneyguru' twice. Mixed in with TestSaveLoadMixin to make sure that loading 
-    # completely wipes out previous transactions (If it doesn't, old transaction end up in the save 
-    # file, making them being re-added to the account in the next load).
-    def setUp(self):
-        self.create_instances()
-        self.document.date_range = MonthRange(date(2008, 2, 1))
-        self.document.load_from_xml(self.filepath('moneyguru', 'simple.moneyguru'))
-        self.document.load_from_xml(self.filepath('moneyguru', 'simple.moneyguru'))
-    
-
-class InvalidAccountType(TestCase):
-    #Loads 'invalid_account_type.moneyguru' which contains a single account with an invalid type.
-    def setUp(self):
-        self.create_instances()
-        self.document.load_from_xml(self.filepath('moneyguru', 'invalid_account_type.moneyguru'))
-    
-    def test_account_type(self):
-        """The account with the invalid type is imported as an asset account."""
-        self.assertEqual(self.bsheet.assets.children_count, 3)
     
 
 class TransferBetweenTwoReferencedAccounts(TestCase):
@@ -510,15 +350,4 @@ class ImportFileWithMultipleTransferReferences(TestCase):
         self.mainwindow.select_income_statement()
         self.assertEqual(self.istatement.income[0].name, 'income')
         self.assertEqual(self.istatement.expenses[0].name, 'expense')
-    
-
-class LoadImportWithTransactionInTheFuture(TestCase):
-    def setUp(self):
-        self.mock_today(2008, 2, 1) # before any txn date
-        self.create_instances()
-        self.document.parse_file_for_import(unicode(self.filepath('moneyguru', 'simple.moneyguru')))
-    
-    def test_transactions_show_up(self):
-        # even when there are txns in the future, they show up in the import panel
-        self.assertEqual(len(self.itable), 2)
     
---
+++ py/tests/load_test.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+# Created By: Virgil Dupras
+# Created On: 2009-12-27
+# $Id$
+# Copyright 2009 Hardcoded Software (http://www.hardcoded.net)
+# 
+# This software is licensed under the "HS" License as described in the "LICENSE" file, 
+# which should be included with this package. The terms are also available at 
+# http://www.hardcoded.net/licenses/hs_license
+
+from datetime import date
+
+from nose.tools import eq_
+
+from hsutil.currency import PLN, CAD
+
+from ..model.date import MonthRange
+from .base import TestCase, TestSaveLoadMixin, TestQIFExportImportMixin
+
+class LoadFile(TestCase):
+    # Loads 'simple.moneyguru', a file with 2 accounts and 2 entries in each. Select the first entry.
+    def setUp(self):
+        self.mock_today(2008, 2, 20) # so that the entries are shown
+        self.create_instances()
+        self.add_account() # This is to set the modified flag to true so we can make sure it has been put back to false
+        self.document.load_from_xml(self.filepath('moneyguru', 'simple.moneyguru'))
+        self.mainwindow.select_balance_sheet()
+        self.bsheet.selected = self.bsheet.assets[0]
+        self.bsheet.show_selected_account()
+    
+    def test_add_entry(self):
+        # Adding an entry sets the modified flag.
+        self.add_entry()
+        assert self.document.is_dirty()
+    
+    def test_change_account_currency(self):
+        # Changing an account currency sets the modified flag.
+        self.apanel.load()
+        self.apanel.currency = PLN
+        self.apanel.save()
+        assert self.document.is_dirty()
+    
+    def test_delete_account(self):
+        # Removing an account sets the modified flag.
+        self.mainwindow.select_balance_sheet()
+        self.bsheet.selected = self.bsheet.assets[0]
+        self.bsheet.delete()
+        self.arpanel.ok() # continue deletion
+        assert self.document.is_dirty()
+    
+    def test_delete_entries(self):
+        # Deleting an entry sets the modified flag.
+        self.etable.delete()
+        assert self.document.is_dirty()
+    
+    def test_delete_transactions(self):
+        # Deleting a transaction sets the modified flag.
+        self.mainwindow.select_transaction_table()
+        self.ttable.select([0]) # will be automatic at some point
+        self.ttable.delete()
+        assert self.document.is_dirty()
+    
+    def test_change_split(self):
+        # Changing a split in the tpanel doesn't make the app dirty unless it's saved.
+        self.etable.select([1])
+        self.tpanel.load()
+        self.stable.delete()
+        assert not self.document.is_dirty()
+    
+    def test_edit_entry(self):
+        # When about to save the document, if an entry is in edition, the document saves the edits first
+        row = self.etable.selected_row
+        row.description = 'foo'
+        self.document.stop_edition()
+        assert self.document.is_dirty() # We started editing, the flag is on
+        self.etable.save_edits()
+        assert self.document.is_dirty()
+    
+    def test_initial_selection(self):
+        # Right after load, the last entry is selected.
+        eq_(self.etable.selected_indexes, [1])
+    
+    def test_not_modified(self):
+        # Loading a file resets the modified flag.
+        assert not self.document.is_dirty()
+    
+    def test_rename_account(self):
+        # Renaming an account sets the modified flag.
+        self.mainwindow.select_balance_sheet()
+        self.bsheet.selected = self.bsheet.assets[0]
+        self.bsheet.selected.name = 'some other name'
+        self.bsheet.save_edits()
+        assert self.document.is_dirty()
+    
+    def test_save_edit_without_edit(self):
+        # Calling save_entry() without having made changes doesn't set the modified flag.
+        self.etable.save_edits()
+        assert not self.document.is_dirty()
+    
+
+class LoadTwice(TestCase, TestSaveLoadMixin):
+    # Loads 'simple.moneyguru' twice. Mixed in with TestSaveLoadMixin to make sure that loading 
+    # completely wipes out previous transactions (If it doesn't, old transaction end up in the save 
+    # file, making them being re-added to the account in the next load).
+    def setUp(self):
+        self.create_instances()
+        self.document.date_range = MonthRange(date(2008, 2, 1))
+        self.document.load_from_xml(self.filepath('moneyguru', 'simple.moneyguru'))
+        self.document.load_from_xml(self.filepath('moneyguru', 'simple.moneyguru'))
+    
+
+class LoadMultiCurrency(TestCase):
+    # Loads 'multi_currency.moneyguru', a file with 2 accounts and a multi-currency transaction.
+    def setUp(self):
+        self.create_instances()
+        self.document.date_range = MonthRange(date(2008, 2, 1))
+        self.document.load_from_xml(self.filepath('moneyguru', 'multi_currency.moneyguru'))
+        self.mainwindow.select_balance_sheet()
+        self.bsheet.selected = self.bsheet.assets[0]
+        self.bsheet.show_selected_account()
+        self.etable.select([0])
+    
+    def test_amounts(self):
+        # The amounts are correctly loaded and the logical imbalance has been dealt with.
+        eq_(self.etable[0].increase, '200.00')
+        self.tpanel.load()
+        eq_(len(self.stable), 4)
+        eq_(self.stable[0].debit, '200.00')
+        eq_(self.stable[1].debit, 'PLN 123.45')
+        expected = set(['200.00', 'PLN 123.45'])
+        assert self.stable[2].credit in expected
+        assert self.stable[3].credit in expected
+    
+
+class LoadPayeeDescription(TestCase):
+    def setUp(self):
+        self.create_instances()
+        self.document.date_range = MonthRange(date(2008, 3, 1))
+        self.document.load_from_xml(self.filepath('moneyguru', 'payee_description.moneyguru'))
+        self.mainwindow.select_transaction_table()
+    
+    def test_attributes(self):
+        # description and payee attributes load correctly, even when some of them are missing.
+        eq_(self.ttable[0].description, 'description')
+        eq_(self.ttable[0].payee, 'payee')
+        eq_(self.ttable[1].description, 'description')
+        eq_(self.ttable[1].payee, '')
+        eq_(self.ttable[2].description, '')
+        eq_(self.ttable[2].payee, 'payee')
+    
+
+class TwoAccountTwoEntriesInEachWithNonAsciiStrings(TestCase, TestSaveLoadMixin, TestQIFExportImportMixin):
+    # Two accounts, two entries in each. Descriptions, categories and account names contain
+    # non-latin characters (a polish 'l'). currencies are set to non-default values. first account
+    # is selected.
+    
+    # Mixin with TestSaveLoadMixin to make sure all those values come back up alright
+    # Same for TestQIFExportImportMixin
+    def setUp(self):
+        self.create_instances()
+        self.add_account(u'first_account\u0142', currency=PLN)
+        self.document.show_selected_account()
+        self.add_entry('3/10/2007', u'first\u0142', transfer='other account', increase='1 usd')
+        self.add_entry('4/10/2007', u'second\u0142', increase='2 usd') # Imbalance
+        self.add_account(u'second_account\u0142', currency=CAD)
+        self.document.show_selected_account()
+        self.add_entry('5/10/2007', u'third\u0142', transfer=u'first_account\u0142', decrease='1 usd')
+        self.add_entry('6/10/2007', u'fourth\u0142', transfer='yet another account', decrease='2 usd')
+    
+
+class LoadInvalidAccountType(TestCase):
+    #Loads 'invalid_account_type.moneyguru' which contains a single account with an invalid type.
+    def setUp(self):
+        self.create_instances()
+        self.document.load_from_xml(self.filepath('moneyguru', 'invalid_account_type.moneyguru'))
+    
+    def test_account_type(self):
+        # The account with the invalid type is imported as an asset account.
+        eq_(self.bsheet.assets.children_count, 3)
+    
+
+class LoadImportWithTransactionInTheFuture(TestCase):
+    def setUp(self):
+        self.mock_today(2008, 2, 1) # before any txn date
+        self.create_instances()
+        self.document.parse_file_for_import(unicode(self.filepath('moneyguru', 'simple.moneyguru')))
+    
+    def test_transactions_show_up(self):
+        # even when there are txns in the future, they show up in the import panel
+        eq_(len(self.itable), 2)
+    
