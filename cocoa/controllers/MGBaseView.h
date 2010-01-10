ee6805ad79beb0b231f54a6c0b3196938d064e47
Virgil Dupras <hsoft@hardcoded.net>
2010-01-10 09:58:52 +0100

    Removed a temporary comment that's no longer relevant.

--- cocoa/controllers/MGBaseView.h
+++ cocoa/controllers/MGBaseView.h
@@ -6,12 +6,6 @@ which should be included with this package. The terms are also available at
 http://www.hardcoded.net/licenses/hs_license
 */
 
-/*
-TEMPORARY UNIT
-This unit is created as a temporary hack for the basic view migration. Will disappear when views
-have correspondant classes down in the core.
-*/
-
 #import <Cocoa/Cocoa.h>
 #import "MGPrintView.h"
 
