e58b54c8690913c79a78bc7297663e3ca4e3bf5c
Virgil Dupras <hsoft@hardcoded.net>
2010-01-11 15:25:26 +0100

    Modernized `tests.gui.account_panel_test`

--- core/tests/gui/account_panel_test.py
+++ core/tests/gui/account_panel_test.py
@@ -10,24 +10,14 @@ from nose.tools import eq_, assert_raises
 
 from hsutil.currency import Currency, CAD
 
-from ..base import TestCase, CommonSetup
+from ..base import TestCase
 from ...exception import OperationAborted
 from ...model.account import AccountType
-
-class CommonSetup(CommonSetup):
-    def setup_accounts_of_all_types(self):
-        # liability created first to force a sorting on the panel side
-        self.add_account_legacy('liability', account_type=AccountType.Liability)
-        self.add_account_legacy('asset', account_type=AccountType.Asset)
-        self.add_account_legacy('income', account_type=AccountType.Income)
-        self.add_account_legacy('expense', account_type=AccountType.Expense)
-        self.mainwindow.select_income_statement()
-    
 
 class SomeAccount(TestCase):
     def setUp(self):
         self.create_instances()
-        self.add_account_legacy('foobar', CAD, account_type=AccountType.Expense)
+        self.add_account('foobar', CAD, account_type=AccountType.Expense)
         self.mainwindow.select_income_statement()
         self.clear_gui_calls()
     
@@ -40,33 +30,33 @@ class SomeAccount(TestCase):
     def test_change_currency_index(self):
         """Changing currency_index correctly updates the currency"""
         self.apanel.currency_index = 0
-        self.assertEqual(self.apanel.currency, Currency.all[0])
+        eq_(self.apanel.currency, Currency.all[0])
         self.apanel.currency_index = 42
-        self.assertEqual(self.apanel.currency, Currency.all[42])
+        eq_(self.apanel.currency, Currency.all[42])
         self.apanel.currency_index = 9999 # doesn't do anything
-        self.assertEqual(self.apanel.currency, Currency.all[42])
-        self.assertEqual(self.apanel.currency_index, 42)
+        eq_(self.apanel.currency, Currency.all[42])
+        eq_(self.apanel.currency_index, 42)
     
     def test_change_type_index(self):
         """Changing type_index correctly updates the type"""
         self.apanel.type_index = 0
-        self.assertEqual(self.apanel.type, AccountType.Asset)
+        eq_(self.apanel.type, AccountType.Asset)
         self.apanel.type_index = 1
-        self.assertEqual(self.apanel.type, AccountType.Liability)
+        eq_(self.apanel.type, AccountType.Liability)
         self.apanel.type_index = 2
-        self.assertEqual(self.apanel.type, AccountType.Income)
+        eq_(self.apanel.type, AccountType.Income)
         self.apanel.type_index = 4 # doesn't do anything
-        self.assertEqual(self.apanel.type, AccountType.Income)
-        self.assertEqual(self.apanel.type_index, 2)
+        eq_(self.apanel.type, AccountType.Income)
+        eq_(self.apanel.type_index, 2)
     
     def test_fields(self):
         """The base field values"""
         self.apanel.load()
-        self.assertEqual(self.apanel.name, 'foobar')
-        self.assertEqual(self.apanel.type, AccountType.Expense)
-        self.assertEqual(self.apanel.currency, CAD)
-        self.assertEqual(self.apanel.type_index, 3) # Expense type is last in the list
-        self.assertEqual(self.apanel.currency_index, Currency.all.index(CAD))
+        eq_(self.apanel.name, 'foobar')
+        eq_(self.apanel.type, AccountType.Expense)
+        eq_(self.apanel.currency, CAD)
+        eq_(self.apanel.type_index, 3) # Expense type is last in the list
+        eq_(self.apanel.currency_index, Currency.all.index(CAD))
     
     def test_fields_before_load(self):
         # ensure no crash occurs
@@ -88,16 +78,16 @@ class SomeAccount(TestCase):
         # To test the currency, we have to load again
         self.istatement.selected = self.istatement.income[0]
         self.apanel.load()
-        self.assertEqual(self.apanel.currency, Currency.all[42])
-        self.assertEqual(self.apanel.type, AccountType.Income)
-        self.assertEqual(self.apanel.name, 'foobaz')
+        eq_(self.apanel.currency, Currency.all[42])
+        eq_(self.apanel.type, AccountType.Income)
+        eq_(self.apanel.name, 'foobaz')
     
 
 class TwoAccounts(TestCase):
     def setUp(self):
         self.create_instances()
-        self.add_account_legacy('foobar')
-        self.add_account_legacy('foobaz')
+        self.add_account('foobar')
+        self.add_account('foobaz')
         self.clear_gui_calls()
     
     def test_duplicate_name(self):
