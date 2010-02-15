b7644000547f48eb00f4857caa4f995a96ca362a
Virgil Dupras <hsoft@hardcoded.net>
2010-02-15 10:43:36 +0100

    Integrated MGWindowController and HSWindowController from cocoalib.

--- cocoa/controllers/MGWindowController.h
+++ cocoa/controllers/MGWindowController.h
@@ -7,17 +7,7 @@ http://www.hardcoded.net/licenses/hs_license
 */
 
 #import <Cocoa/Cocoa.h>
-#import "PyGUI.h"
+#import "HSWindowController.h"
 
-// You, it would be nice if objc had multiple inheritance...
-// We need the window controllers to be NSWindowController instances if we want the window dealloc
-// to be properly made when the document closes.
-
-@interface MGWindowController : NSWindowController
-{
-    PyGUI *py;
-}
-- (id)initWithNibName:(NSString *)aNibName pyClassName:(NSString *)aClassName pyParent:(id)aPyParent;
-- (void)connect;
-- (void)disconnect;
+@interface MGWindowController : HSWindowController
 @end
--- cocoa/controllers/MGWindowController.m
+++ cocoa/controllers/MGWindowController.m
@@ -7,50 +7,12 @@ http://www.hardcoded.net/licenses/hs_license
 */
 
 #import "MGWindowController.h"
-#import "MGUtils.h"
 
 @implementation MGWindowController
-- (id)initWithNibName:(NSString *)aNibName pyClassName:(NSString *)aClassName pyParent:(id)aPyParent;
-{
-    self = [super initWithWindowNibName:aNibName];
-    Class pyClass = [MGUtils classNamed:aClassName];
-    py = [[pyClass alloc] initWithCocoa:self pyParent:aPyParent];
-    return self;
-}
-
-- (oneway void)release
-{
-    // see MGGUIController
-    if ([self retainCount] == 2)
-    {
-        [py free];
-    }
-    [super release];
-}
-
-- (void)dealloc
-{
-    // NSLog([NSString stringWithFormat:@"%@ dealloc",[[self class] description]]);
-    [py release];
-    [super dealloc];
-}
-
-- (void)connect
-{
-    [py connect];
-}
-
-- (void)disconnect
-{
-    [py disconnect];
-}
-
 // Python --> Cocoa
-
 - (void)showMessage:(NSString *)msg
 {
     NSAlert *a = [NSAlert alertWithMessageText:msg defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
     [a beginSheetModalForWindow:[self window] modalDelegate:self didEndSelector:nil contextInfo:nil];
 }
-
 @end
