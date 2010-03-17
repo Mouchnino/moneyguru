f9af78071073c0079ae464dfbb4731126eda9620
Virgil Dupras <hsoft@hardcoded.net>
2010-03-17 10:33:11 +0100

    Nose-ified custom_date_range_panel_test.

--- core/tests/gui/custom_date_range_panel_test.py
+++ core/tests/gui/custom_date_range_panel_test.py
@@ -8,28 +8,32 @@
 
 from datetime import date
 
-from ..base import TestCase
+from nose.tools import eq_
+
+from ..base import TestApp, with_app
 from ...model.date import MonthRange
 
-class DateRangeOnOctober2007AndPanelLoaded(TestCase):
-    def setUp(self):
-        self.create_instances()
-        self.document.date_range = MonthRange(date(2007, 10, 1))
-        self.drsel.select_custom_date_range()
-        self.clear_gui_calls()
-    
-    def test_dates(self):
-        # the dates, when loading the panel, are the dates of the current date range
-        self.assertEqual(self.cdrpanel.start_date, '01/10/2007')
-        self.assertEqual(self.cdrpanel.end_date, '31/10/2007')
-    
-    def test_set_start_later_than_end(self):
-        # when start_date is set later than end_date, end_date is set to the same date.
-        self.cdrpanel.start_date = '04/11/2007'
-        self.assertEqual(self.cdrpanel.end_date, '04/11/2007')
-    
-    def test_set_end_earlier_than_start(self):
-        # when end_date is set earlier than start_date, start_date is set to the same date.
-        self.cdrpanel.end_date = '04/09/2007'
-        self.assertEqual(self.cdrpanel.start_date, '04/09/2007')
-    
+#--- Month range
+def app_monthrange():
+    app = TestApp()
+    app.doc.date_range = MonthRange(date(2007, 10, 1))
+    app.drsel.select_custom_date_range()
+    return app
+
+@with_app(app_monthrange)
+def test_dates(app):
+    # the dates, when loading the panel, are the dates of the current date range
+    eq_(app.cdrpanel.start_date, '01/10/2007')
+    eq_(app.cdrpanel.end_date, '31/10/2007')
+
+@with_app(app_monthrange)
+def test_set_start_later_than_end(app):
+    # when start_date is set later than end_date, end_date is set to the same date.
+    app.cdrpanel.start_date = '04/11/2007'
+    eq_(app.cdrpanel.end_date, '04/11/2007')
+
+@with_app(app_monthrange)
+def test_set_end_earlier_than_start(app):
+    # when end_date is set earlier than start_date, start_date is set to the same date.
+    app.cdrpanel.end_date = '04/09/2007'
+    eq_(app.cdrpanel.start_date, '04/09/2007')
