1ceac87af48cf52ea730efaf173cf6cf07cd4275
Virgil Dupras <hsoft@hardcoded.net>
2010-03-19 17:28:28 +0100

    Fixed crash on exit under Qt since PyQt 4.7.2.

--- qt/start.py
+++ qt/start.py
@@ -9,6 +9,7 @@
 # http://www.hardcoded.net/licenses/hs_license
 
 import sys
+import gc
 
 from PyQt4.QtCore import QFile, QTextStream
 from PyQt4.QtGui import QApplication, QIcon, QPixmap
@@ -30,4 +31,12 @@ if __name__ == "__main__":
     app.setStyleSheet(style)
     mgapp = MoneyGuru()
     install_excepthook()
-    sys.exit(app.exec_())
+    exec_result = app.exec_()
+    del mgapp
+    # Since PyQt 4.7.2, I had crashes on exit, and from reading the mailing list, it seems to be
+    # caused by some weird crap about C++ instance being deleted with python instance still living.
+    # The worst part is that Phil seems to say this is expected behavior. So, whatever, this
+    # gc.collect() below is required to avoid a crash.
+    gc.collect()
+    del app
+    sys.exit(exec_result)
