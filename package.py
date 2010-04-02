8e9338d280be685fdbe88996b0fdb8fc1d4fcd4d
Virgil Dupras <hsoft@hardcoded.net>
2010-04-02 07:17:18 -0700

    Added menu item to the debian package.

--- package.py
+++ package.py
@@ -37,6 +37,7 @@ def package_debian():
     shutil.copytree('debian', op.join(destpath, 'debian'))
     build_debian_changelog(op.join('help', 'changelog.yaml'), op.join(destpath, 'debian', 'changelog'), 'moneyguru', from_version='1.8.0')
     shutil.copytree(op.join('help', 'moneyguru_help'), op.join(srcpath, 'help'))
+    shutil.copy(op.join('images', 'logo_small.png'), srcpath)
     compileall.compile_dir(srcpath)
     os.chdir(destpath)
     os.system("dpkg-buildpackage")
