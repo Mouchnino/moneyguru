733351d1d29a4b177e5f9fda28103902b31979bf
Virgil Dupras <hsoft@hardcoded.net>
2010-04-12 14:57:27 +0200

    Under Qt, added support for osx.

--- qt/plat_osx.py
+++ qt/plat_osx.py
@@ -1,20 +1,10 @@
 # -*- coding: utf-8 -*-
 # Created By: Virgil Dupras
-# Created On: 2010-04-02
+# Created On: 2010-04-12
 # Copyright 2010 Hardcoded Software (http://www.hardcoded.net)
 # 
 # This software is licensed under the "HS" License as described in the "LICENSE" file, 
 # which should be included with this package. The terms are also available at 
 # http://www.hardcoded.net/licenses/hs_license
 
-import sys
-
-if sys.platform == 'win32':
-    from plat_win import *
-elif sys.platform == 'linux2':
-    from plat_lnx import *
-elif sys.platform == 'darwin':
-    from plat_osx import *
-else:
-    print "Unsupported platform"
-
+HELP_PATH = 'help'
