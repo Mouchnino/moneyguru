c0f1b77f5eff0eea28e06d55c5b7223b043d0dc4
Virgil Dupras <hsoft@hardcoded.net>
2010-06-25 10:49:53 +0200

    Nose-ified account_reassign_panel_test.

--- core/tests/gui/account_reassign_panel_test.py
+++ core/tests/gui/account_reassign_panel_test.py
@@ -6,35 +6,40 @@
 # which should be included with this package. The terms are also available at 
 # http://www.hardcoded.net/licenses/hs_license
 
-from ..base import TestCase, CommonSetup
+from nose.tools import eq_
 
-class DeletingSecondAccount(TestCase, CommonSetup):
-    def setUp(self):
-        self.create_instances()
-        self.add_accounts('one', 'two')
-        self.mainwindow.show_account()
-        self.add_entry(transfer='three', increase='42')
-        self.mainwindow.select_balance_sheet()
-        self.bsheet.selected = self.bsheet.assets[1] # account 'two', the one with the entry
-        self.bsheet.delete() # the panel shows up
-    
-    def test_available_accounts(self):
-        # the available accounts in arpanel are No Account, one and three
-        expected = ['No Account', 'one', 'three']
-        self.assertEqual(self.arpanel.available_accounts, expected)
-    
-    def test_no_delete_took_place(self):
-        # the panel shows up, but no deletion takes place yet.
-        self.assertEqual(len(self.bsheet.assets), 4) # 2 + total lines.
-    
-    def test_reassign_to_one(self):
-        # choosing 'one' and continuing reassigns two's entry to one.
-        self.arpanel.account_index = 1 # one
-        self.arpanel.save()
-        self.assertEqual(len(self.bsheet.assets), 3) # now, it's deleted
-        self.bsheet.selected = self.bsheet.assets[0]
-        self.bsheet.show_selected_account()
-        self.assertEqual(self.ta.etable_count(), 1)
-        self.assertEqual(self.etable[0].transfer, 'three')
-        self.assertEqual(self.etable[0].increase, '42.00') # got the right side of the txn
-    
+from ..base import TestApp, with_app
+
+#--- Deleting second account
+def app_deleting_second_account():
+    app = TestApp()
+    app.add_accounts('one', 'two')
+    app.mw.show_account()
+    app.add_entry(transfer='three', increase='42')
+    app.mw.select_balance_sheet()
+    app.bsheet.selected = app.bsheet.assets[1] # account 'two', the one with the entry
+    app.bsheet.delete() # the panel shows up
+    return app
+
+@with_app(app_deleting_second_account)
+def test_available_accounts(app):
+    # the available accounts in arpanel are No Account, one and three
+    expected = ['No Account', 'one', 'three']
+    eq_(app.arpanel.available_accounts, expected)
+
+@with_app(app_deleting_second_account)
+def test_no_delete_took_place(app):
+    # the panel shows up, but no deletion takes place yet.
+    eq_(len(app.bsheet.assets), 4) # 2 + total lines.
+
+@with_app(app_deleting_second_account)
+def test_reassign_to_one(app):
+    # choosing 'one' and continuing reassigns two's entry to one.
+    app.arpanel.account_index = 1 # one
+    app.arpanel.save()
+    eq_(len(app.bsheet.assets), 3) # now, it's deleted
+    app.bsheet.selected = app.bsheet.assets[0]
+    app.bsheet.show_selected_account()
+    eq_(app.etable_count(), 1)
+    eq_(app.etable[0].transfer, 'three')
+    eq_(app.etable[0].increase, '42.00') # got the right side of the txn
