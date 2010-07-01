5d89f6926bc46091b0a24f077a5eefa64ebdf832
Virgil Dupras <hsoft@hardcoded.net>
2010-07-01 12:03:23 +0200

    Nose-ified search_field_test.

--- core/tests/gui/search_field_test.py
+++ core/tests/gui/search_field_test.py
@@ -8,200 +8,193 @@
 
 from nose.tools import eq_
 
-from ..base import TestCase, CommonSetup as CommonSetupBase
+from ..base import TestApp, with_app
 
-class CommonSetup(CommonSetupBase):
-    def setup_two_transactions(self):
-        self.add_account_legacy('Desjardins')
-        self.add_entry(description='a Deposit', payee='Joe SixPack', checkno='42A', transfer='Income', increase='212.12')
-        # it's important for the test that this txns has no space in its fields
-        self.add_entry(description='Withdrawal', payee='Dunno-What-To-Write', checkno='24B', transfer='Cash', decrease='140')
-        self.mainwindow.select_transaction_table()
-    
+#--- Pristine
+@with_app(TestApp)    
+def test_set_query(app):
+    # Setting the 'query' property works"""
+    app.sfield.query = 'foobar'
+    eq_(app.sfield.query, 'foobar')
 
-class Pristine(TestCase):
-    def setUp(self):
-        self.create_instances()
-    
-    def test_set_query(self):
-        """Setting the 'query' propsert works"""
-        self.sfield.query = 'foobar'
-        self.assertEqual(self.sfield.query, 'foobar')
-    
+#--- Two transactions
+def app_two_transactions():
+    app = TestApp()
+    app.add_account('Desjardins')
+    app.mw.show_account()
+    app.add_entry(description='a Deposit', payee='Joe SixPack', checkno='42A', transfer='Income', increase='212.12')
+    # it's important for the test that this txns has no space in its fields
+    app.add_entry(description='Withdrawal', payee='Dunno-What-To-Write', checkno='24B', transfer='Cash', decrease='140')
+    app.mw.select_transaction_table()
+    return app
 
-class TwoTransactions(TestCase, CommonSetup):
-    def setUp(self):
-        self.create_instances()
-        self.setup_two_transactions()
-    
-    def test_account(self):
-        # when using the 'account:' search form, only account are searched. Also, commas can be used
-        # to specify more than one term
-        self.sfield.query = 'account: withdrawal,inCome'
-        self.assertEqual(self.ttable.row_count, 1)
-        self.assertEqual(self.ttable[0].description, 'a Deposit')
-    
-    def test_query_amount(self):
-        """Amounts can be queried, and the cents, when 0, can be ommited"""
-        self.sfield.query = '140'
-        self.assertEqual(self.ttable.row_count, 1)
-        self.assertEqual(self.ttable[0].description, 'Withdrawal')
-    
-    def test_query_amount_exact(self):
-        """Amount searches are exact"""
-        self.sfield.query = '212'
-        self.assertEqual(self.ttable.row_count, 0)
-        self.sfield.query = '212.12'
-        self.assertEqual(self.ttable.row_count, 1)
-        self.assertEqual(self.ttable[0].description, 'a Deposit')
-    
-    def test_query_amount_negative(self):
-        """When searching for amount, we ignore the amounts' sign"""
-        self.sfield.query = '-140'
-        self.assertEqual(self.ttable.row_count, 1)
-        self.assertEqual(self.ttable[0].description, 'Withdrawal')
-    
-    def test_query_description(self):
-        """The query is case insensitive and works on description"""
-        self.sfield.query = 'wiTH'
-        self.assertEqual(self.ttable.row_count, 1)
-        self.assertEqual(self.ttable[0].description, 'Withdrawal')
-    
-    def test_query_checkno(self):
-        """The query works on checkno"""
-        self.sfield.query = '42a'
-        self.assertEqual(self.ttable.row_count, 1)
-        self.assertEqual(self.ttable[0].description, 'a Deposit')
-    
-    def test_query_checkno_partial(self):
-        """We don't match transactions that only partially match checkno (it doesn't make much sense)"""
-        self.sfield.query = '4'
-        self.assertEqual(self.ttable.row_count, 0)
-    
-    def test_query_from(self):
-        """The 'from' account can be queried"""
-        self.sfield.query = 'desJ'
-        self.assertEqual(self.ttable.row_count, 2)
-    
-    def test_query_payee(self):
-        """The query is case insensitive and works on payee"""
-        self.sfield.query = 'siX'
-        self.assertEqual(self.ttable.row_count, 1)
-        self.assertEqual(self.ttable[0].description, 'a Deposit')
-    
-    def test_query_space(self):
-        # Querying for a space character doesn't cause a crash. Previously, it did because it was
-        # parsed as an amount.
-        self.mainwindow.select_transaction_table()
-        self.sfield.query = ' ' # no crash
-        self.assertEqual(self.ttable.row_count, 2) # same as no filter
-    
-    def test_query_to(self):
-        """The 'to' account can be queried"""
-        self.sfield.query = 'Come'
-        self.assertEqual(self.ttable.row_count, 1)
-        self.assertEqual(self.ttable[0].description, 'a Deposit')
-    
+@with_app(app_two_transactions)
+def test_account(app):
+    # when using the 'account:' search form, only account are searched. Also, commas can be used
+    # to specify more than one term
+    app.sfield.query = 'account: withdrawal,inCome'
+    eq_(app.ttable.row_count, 1)
+    eq_(app.ttable[0].description, 'a Deposit')
 
-class ThreeTransactionsOneWithZeroAmount(TestCase, CommonSetup):
-    def setUp(self):
-        self.create_instances()
-        self.setup_two_transactions()
-        self.add_entry(description='zero-amount')
-    
-    def test_query_amount(self):
-        # querying an amount with a zero amount in the stack doesn't cause a crash
-        self.sfield.query = '212.12' # no crash
-        self.assertEqual(self.ttable.row_count, 1)
-    
-    
-class Split(TestCase):
-    def setUp(self):
-        self.create_instances()
-        self.mainwindow.select_transaction_table()
-        self.ttable.add()
-        self.tpanel.load()
-        # Here, we're creating a split that looks like:
-        # first       42  0
-        # second       0 42
-        # third       12  0
-        # Unasssigned  0 12
-        self.stable[0].account = 'first'
-        self.stable[0].memo = 'memo1'
-        self.stable[0].debit = '42'
-        self.stable.save_edits()
-        self.stable.select([1])
-        self.stable[1].account = 'second'
-        self.stable[1].memo = 'memo2'
-        self.stable.save_edits()
-        self.stable.add()
-        self.stable[2].account = 'third'
-        self.stable[2].debit = '12'
-        self.stable.save_edits()
-        self.tpanel.save()
-    
-    def test_query_memo(self):
-        # memo fields are part of the search query
-        self.sfield.query = 'memo2'
-        self.assertEqual(self.ttable.row_count, 1)
-    
-    def test_query_split_account(self):
-        """Any account in a split can match a sfield query"""
-        self.sfield.query = 'third'
-        self.assertEqual(self.ttable.row_count, 1)
-    
+@with_app(app_two_transactions)
+def test_query_amount(app):
+    # Amounts can be queried, and the cents, when 0, can be ommited.
+    app.sfield.query = '140'
+    eq_(app.ttable.row_count, 1)
+    eq_(app.ttable[0].description, 'Withdrawal')
 
-class ThreeTransactionsFiltered(TestCase):
-    def setUp(self):
-        self.create_instances()
-        self.add_account_legacy('some account')
-        self.add_entry(description='foo')
-        self.add_entry(description='bar')
-        self.add_entry(description='bar')
-        self.mainwindow.select_transaction_table()
-        self.sfield.query = 'bar'
-        self.clear_gui_calls()
-    
-    def test_change_account(self):
-        """Changing selection to another account cancels the filter"""
-        self.mainwindow.select_balance_sheet()
-        self.assertEqual(self.sfield.query, '')
-        # setting the sfield query didn't make document go to all_transactions again
-        eq_(self.mainwindow.current_pane_index, 0)
-        self.check_gui_calls(self.sfield_gui, ['refresh'])
-        self.mainwindow.select_transaction_table()
-        self.assertEqual(self.ttable.row_count, 3)
-    
-    def test_change_account_to_bsheet(self):
-        """Balance sheet is another notification, so we must also test it in addition to 
-        test_change_account.
-        """
-        self.mainwindow.select_balance_sheet()
-        self.assertEqual(self.sfield.query, '')
-        self.check_gui_calls(self.sfield_gui, ['refresh'])
-        self.mainwindow.select_transaction_table()
-        self.assertEqual(self.ttable.row_count, 3)
-    
-    def test_modify_transaction_out_of_filter(self):
-        """When changing a txn so it doesn't match the filter anymore, remove it"""
-        row = self.ttable.selected_row
-        row.description = 'baz'
-        self.ttable.save_edits()
-        self.assertEqual(self.ttable.row_count, 1)
-        self.assertEqual(self.ttable.selected_indexes, [0])
-    
+@with_app(app_two_transactions)
+def test_query_amount_exact(app):
+    # Amount searches are exact.
+    app.sfield.query = '212'
+    eq_(app.ttable.row_count, 0)
+    app.sfield.query = '212.12'
+    eq_(app.ttable.row_count, 1)
+    eq_(app.ttable[0].description, 'a Deposit')
 
-class GroupedAndUngroupedTransactions(TestCase):
-    def setUp(self):
-        self.create_instances()
-        self.add_group('MyGroup')
-        self.add_account_legacy('Grouped', group_name='MyGroup')
-        self.add_account_legacy('Ungrouped')
-        self.add_txn(description='first', from_='Grouped')
-        self.add_txn(description='second', from_='Ungrouped')
-    
-    def test_query_group(self):
-        self.sfield.query = 'group:foo,mygRoup'
-        self.assertEqual(self.ttable.row_count, 1)
-        self.assertEqual(self.ttable[0].description, 'first')
-    
+@with_app(app_two_transactions)
+def test_query_amount_negative(app):
+    # When searching for amount, we ignore the amounts' sign.
+    app.sfield.query = '-140'
+    eq_(app.ttable.row_count, 1)
+    eq_(app.ttable[0].description, 'Withdrawal')
+
+@with_app(app_two_transactions)
+def test_query_description(app):
+    # The query is case insensitive and works on description.
+    app.sfield.query = 'wiTH'
+    eq_(app.ttable.row_count, 1)
+    eq_(app.ttable[0].description, 'Withdrawal')
+
+@with_app(app_two_transactions)
+def test_query_checkno(app):
+    # The query works on checkno.
+    app.sfield.query = '42a'
+    eq_(app.ttable.row_count, 1)
+    eq_(app.ttable[0].description, 'a Deposit')
+
+@with_app(app_two_transactions)
+def test_query_checkno_partial(app):
+    # We don't match transactions that only partially match checkno (it doesn't make much sense).
+    app.sfield.query = '4'
+    eq_(app.ttable.row_count, 0)
+
+@with_app(app_two_transactions)
+def test_query_from(app):
+    # The 'from' account can be queried.
+    app.sfield.query = 'desJ'
+    eq_(app.ttable.row_count, 2)
+
+@with_app(app_two_transactions)
+def test_query_payee(app):
+    # The query is case insensitive and works on payee.
+    app.sfield.query = 'siX'
+    eq_(app.ttable.row_count, 1)
+    eq_(app.ttable[0].description, 'a Deposit')
+
+@with_app(app_two_transactions)
+def test_query_space(app):
+    # Querying for a space character doesn't cause a crash. Previously, it did because it was
+    # parsed as an amount.
+    app.mw.select_transaction_table()
+    app.sfield.query = ' ' # no crash
+    eq_(app.ttable.row_count, 2) # same as no filter
+
+@with_app(app_two_transactions)
+def test_query_to(app):
+    # The 'to' account can be queried.
+    app.sfield.query = 'Come'
+    eq_(app.ttable.row_count, 1)
+    eq_(app.ttable[0].description, 'a Deposit')
+
+#--- Three txns with zero amount
+def app_three_txns_with_zero_amount():
+    app = TestApp()
+    app.add_txn(description='foo', amount='212.12')
+    app.add_txn(description='bar', amount='140')
+    app.add_txn(description='zero-amount', amount='0')
+    return app
+
+@with_app(app_three_txns_with_zero_amount)
+def test_query_amount_with_zero_amount_txn(app):
+    # querying an amount with a zero amount in the stack doesn't cause a crash
+    app.sfield.query = '212.12' # no crash
+    eq_(app.ttable.row_count, 1)
+
+#--- Split
+def app_split():
+    app = TestApp()
+    splits = [
+        ('first', 'memo1', '42', ''),
+        ('second', 'memo2', '', '42'),
+        ('third', '', '12', ''),
+    ]
+    app.add_txn_with_splits(splits)
+    return app
+
+@with_app(app_split)    
+def test_query_memo(app):
+    # memo fields are part of the search query
+    app.sfield.query = 'memo2'
+    eq_(app.ttable.row_count, 1)
+
+@with_app(app_split) 
+def test_query_split_account(app):
+    # Any account in a split can match a sfield query.
+    app.sfield.query = 'third'
+    eq_(app.ttable.row_count, 1)
+
+#--- Three txns filtered
+def app_three_txns_filtered():
+    app = TestApp()
+    app.add_txn(description='foo')
+    app.add_txn(description='bar')
+    app.add_txn(description='bar')
+    app.sfield.query = 'bar'
+    app.clear_gui_calls()
+    return app
+
+@with_app(app_three_txns_filtered)
+def test_change_account(app):
+    # Changing selection to another account cancels the filter.
+    app.mainwindow.select_balance_sheet()
+    eq_(app.sfield.query, '')
+    # setting the sfield query didn't make document go to all_transactions again
+    eq_(app.mainwindow.current_pane_index, 0)
+    app.check_gui_calls(app.sfield_gui, ['refresh'])
+    app.mainwindow.select_transaction_table()
+    eq_(app.ttable.row_count, 3)
+
+@with_app(app_three_txns_filtered)
+def test_change_account_to_bsheet(app):
+    # Balance sheet is another notification, so we must also test it in addition to test_change_account.
+    app.mainwindow.select_balance_sheet()
+    eq_(app.sfield.query, '')
+    app.check_gui_calls(app.sfield_gui, ['refresh'])
+    app.mainwindow.select_transaction_table()
+    eq_(app.ttable.row_count, 3)
+
+@with_app(app_three_txns_filtered)
+def test_modify_transaction_out_of_filter(app):
+    # When changing a txn so it doesn't match the filter anymore, remove it.
+    row = app.ttable.selected_row
+    row.description = 'baz'
+    app.ttable.save_edits()
+    eq_(app.ttable.row_count, 1)
+    eq_(app.ttable.selected_indexes, [0])
+
+#--- Grouped and ungrouped txns
+def app_grouped_and_ungrouped_txns():
+    app = TestApp()
+    app.add_group('MyGroup')
+    app.add_account('Grouped', group_name='MyGroup')
+    app.add_account('Ungrouped')
+    app.add_txn(description='first', from_='Grouped')
+    app.add_txn(description='second', from_='Ungrouped')
+    return app
+
+@with_app(app_grouped_and_ungrouped_txns)
+def test_query_group(app):
+    app.sfield.query = 'group:foo,mygRoup'
+    eq_(app.ttable.row_count, 1)
+    eq_(app.ttable[0].description, 'first')
