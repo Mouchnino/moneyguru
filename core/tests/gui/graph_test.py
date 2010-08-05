bb48f039c800385efd139a9fcae925b4b2c106b1
Virgil Dupras <hsoft@hardcoded.net>
2010-08-05 09:36:42 +0200

    Pytest-ified graph_test.

--- core/tests/gui/graph_test.py
+++ core/tests/gui/graph_test.py
@@ -6,32 +6,31 @@
 # which should be included with this package. The terms are also available at 
 # http://www.hardcoded.net/licenses/hs_license
 
-from ..base import TestCase
+from hsutil.testutil import eq_
 
-class Pristine(TestCase):
-    def setUp(self):
-        self.create_instances()
-    
-    def test_monthly_xtickmark_dont_start_at_zero(self):
-        # same as yearly, but with a month based date range
-        self.drsel.select_custom_date_range()
-        self.cdrpanel.start_date = '09/11/2007' # 22 days before the end of the month
-        self.cdrpanel.end_date = '18/02/2008'
-        self.cdrpanel.save() # changes the date range
-        first_mark = self.nwgraph.xtickmarks[1] # 0 is xmin
-        self.assertEqual(first_mark - self.nwgraph.xmin, 22)
-    
-    def test_yearly_xtickmark_dont_start_at_zero(self):
-        # When you have a tickmark range that don't start at 0, don't make the whol X scale slide
-        # rightwards. For example, if you had a multi-year range that don't start at the beginning
-        # of the year, the first xtickmark would still be 365 days on the X line, making the data
-        # and the marks not fitting together.
-        self.drsel.select_custom_date_range()
-        self.cdrpanel.start_date = '09/12/2007' # 23 days before the end of the year
-        self.cdrpanel.end_date = '18/02/2009'
-        self.cdrpanel.save() # changes the date range
-        first_mark = self.nwgraph.xtickmarks[1] # 0 is xmin
-        self.assertEqual(first_mark - self.nwgraph.xmin, 23)
-        # also, don't make the label go left of the xmin
-        self.assertTrue(self.nwgraph.xlabels[0]['pos'] >= self.nwgraph.xmin)
-    
+from ..base import TestApp, with_app
+
+@with_app(TestApp)
+def test_monthly_xtickmark_dont_start_at_zero(app):
+    # same as yearly, but with a month based date range
+    app.drsel.select_custom_date_range()
+    app.cdrpanel.start_date = '09/11/2007' # 22 days before the end of the month
+    app.cdrpanel.end_date = '18/02/2008'
+    app.cdrpanel.save() # changes the date range
+    first_mark = app.nwgraph.xtickmarks[1] # 0 is xmin
+    eq_(first_mark - app.nwgraph.xmin, 22)
+
+@with_app(TestApp)
+def test_yearly_xtickmark_dont_start_at_zero(app):
+    # When you have a tickmark range that don't start at 0, don't make the whol X scale slide
+    # rightwards. For example, if you had a multi-year range that don't start at the beginning
+    # of the year, the first xtickmark would still be 365 days on the X line, making the data
+    # and the marks not fitting together.
+    app.drsel.select_custom_date_range()
+    app.cdrpanel.start_date = '09/12/2007' # 23 days before the end of the year
+    app.cdrpanel.end_date = '18/02/2009'
+    app.cdrpanel.save() # changes the date range
+    first_mark = app.nwgraph.xtickmarks[1] # 0 is xmin
+    eq_(first_mark - app.nwgraph.xmin, 23)
+    # also, don't make the label go left of the xmin
+    assert app.nwgraph.xlabels[0]['pos'] >= app.nwgraph.xmin
