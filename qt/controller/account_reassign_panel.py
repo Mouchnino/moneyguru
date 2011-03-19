0fc540f98937e1d465e2335faf5950b14a0cb9d7
Virgil Dupras <hsoft@hardcoded.net>
2011-03-19 10:59:11 +0100

    Oops, the cancel button in account reassign panel should acually cancel.

--- qt/controller/account_reassign_panel.py
+++ qt/controller/account_reassign_panel.py
@@ -29,7 +29,7 @@ class AccountReassignPanel(Panel):
         self.model = AccountReassignPanelModel(view=self, mainwindow=mainwindow.model)
         
         self.continueButton.clicked.connect(self.accept)
-        self.cancelButton.clicked.connect(self.accept)
+        self.cancelButton.clicked.connect(self.reject)
     
     def _setupUi(self):
         if plat.isWindows():
