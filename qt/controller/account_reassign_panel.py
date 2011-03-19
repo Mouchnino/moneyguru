214c416b103fb247de27aa2677324b54dcb53893
Virgil Dupras <hsoft@hardcoded.net>
2011-03-19 10:34:52 +0100

    De-ui-ified account_reassign_panel and made it a bit larger to accomodate german translation.

--- qt/controller/account_reassign_panel.py
+++ qt/controller/account_reassign_panel.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Created By: Virgil Dupras
 # Created On: 2009-12-02
 # Copyright 2010 Hardcoded Software (http://www.hardcoded.net)
@@ -7,24 +6,71 @@
 # which should be included with this package. The terms are also available at 
 # http://www.hardcoded.net/licenses/bsd_license
 
-from PyQt4.QtGui import QDialog
+from PyQt4.QtCore import QSize
+from PyQt4.QtGui import (QVBoxLayout, QHBoxLayout, QLabel, QComboBox, QSizePolicy, QSpacerItem,
+    QPushButton)
 
+from hscommon.trans import tr as trbase
 from core.gui.account_reassign_panel import AccountReassignPanel as AccountReassignPanelModel
 
 from .panel import Panel
-from ..ui.account_reassign_panel_ui import Ui_AccountReassignPanel
 
-class AccountReassignPanel(Panel, Ui_AccountReassignPanel):
+tr = lambda s: trbase(s, "AccountReassignPanel")
+
+class AccountReassignPanel(Panel):
     FIELDS = [
         ('accountComboBox', 'account_index'),
     ]
     
     def __init__(self, parent, mainwindow):
         Panel.__init__(self, parent)
-        self.setupUi(self)
+        self._setupUi()
         self.model = AccountReassignPanelModel(view=self, mainwindow=mainwindow.model)
+        
+        self.continueButton.clicked.connect(self.accept)
+        self.cancelButton.clicked.connect(self.accept)
+    
+    def _setupUi(self):
+        self.resize(340, 165)
+        self.setWindowTitle(tr("Re-assign Account"))
+        self.verticalLayout = QVBoxLayout(self)
+        self.label = QLabel(self)
+        self.label.setWordWrap(True)
+        self.label.setText(tr("You\'re about to delete a non-empty account. Select an account to re-assign its transactions to."))
+        self.verticalLayout.addWidget(self.label)
+        self.accountComboBox = QComboBox(self)
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.accountComboBox.sizePolicy().hasHeightForWidth())
+        self.accountComboBox.setSizePolicy(sizePolicy)
+        self.accountComboBox.setMinimumSize(QSize(200, 0))
+        self.verticalLayout.addWidget(self.accountComboBox)
+        spacerItem = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+        self.horizontalLayout = QHBoxLayout()
+        spacerItem1 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.cancelButton = QPushButton(self)
+        self.cancelButton.setText(tr("Cancel"))
+        self.cancelButton.setShortcut("Esc")
+        self.horizontalLayout.addWidget(self.cancelButton)
+        self.continueButton = QPushButton(self)
+        self.continueButton.setDefault(True)
+        self.continueButton.setText(tr("Continue"))
+        self.horizontalLayout.addWidget(self.continueButton)
+        self.verticalLayout.addLayout(self.horizontalLayout)
     
     def _loadFields(self):
         self._changeComboBoxItems(self.accountComboBox, self.model.available_accounts)
         Panel._loadFields(self)
     
+
+if __name__ == '__main__':
+    import sys
+    from PyQt4.QtGui import QApplication, QDialog
+    app = QApplication([])
+    dialog = QDialog(None)
+    AccountReassignPanel._setupUi(dialog)
+    dialog.show()
+    sys.exit(app.exec_())
