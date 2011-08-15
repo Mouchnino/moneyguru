2e640a90be1d0293199e6f2b76f69477dbb723f6
Virgil Dupras <hsoft@hardcoded.net>
2011-08-15 08:00:32 -0400

    v2.3.10

--- core/app.py
+++ core/app.py
@@ -33,7 +33,7 @@ SavedCustomRange = namedtuple('SavedCustomRange', 'name start end')
 
 class Application(Broadcaster, RegistrableApplication):
     APP_NAME = "moneyGuru"
-    VERSION = '2.3.9'
+    VERSION = '2.3.10'
     
     def __init__(self, view, date_format='dd/MM/yyyy', decimal_sep='.', grouping_sep='', 
         default_currency=USD, cache_path=None):
