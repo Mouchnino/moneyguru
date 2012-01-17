4eb9cc35357da0b08cf721d4e54d41634d2ada4f
Virgil Dupras <hsoft@hardcoded.net>
2012-01-17 10:50:09 -0500

    [#292 state:fixed] Re-added mass editing dialog labels which went missing after the de-ui-fication.

--- qt/controller/mass_edition_panel.py
+++ qt/controller/mass_edition_panel.py
@@ -9,10 +9,13 @@
 from PyQt4 import QtCore, QtGui
 
 from hscommon.currency import Currency
+from hscommon.trans import trget
 
 from ..support.date_edit import DateEdit
 from ..support.completable_edit import PayeeEdit, AccountEdit, DescriptionEdit
 from .panel import Panel
+
+tr = trget('ui')
 
 class MassEditionPanel(Panel):
     FIELDS = [
@@ -50,7 +53,7 @@ class MassEditionPanel(Panel):
         self.verticalLayout = QtGui.QVBoxLayout(self)
         self.formLayout = QtGui.QFormLayout()
         self.formLayout.setFieldGrowthPolicy(QtGui.QFormLayout.ExpandingFieldsGrow)
-        self.label = QtGui.QLabel(self)
+        self.label = QtGui.QLabel(tr("Date:"), self)
         self.formLayout.setWidget(0, QtGui.QFormLayout.LabelRole, self.label)
         self.horizontalLayout = QtGui.QHBoxLayout()
         self.dateCheckBox = QtGui.QCheckBox(self)
@@ -58,7 +61,7 @@ class MassEditionPanel(Panel):
         self.dateEdit = DateEdit(self)
         self.horizontalLayout.addWidget(self.dateEdit)
         self.formLayout.setLayout(0, QtGui.QFormLayout.FieldRole, self.horizontalLayout)
-        self.label_2 = QtGui.QLabel(self)
+        self.label_2 = QtGui.QLabel(tr("Description:"), self)
         self.formLayout.setWidget(1, QtGui.QFormLayout.LabelRole, self.label_2)
         self.horizontalLayout_2 = QtGui.QHBoxLayout()
         self.descriptionCheckBox = QtGui.QCheckBox(self)
@@ -66,7 +69,7 @@ class MassEditionPanel(Panel):
         self.descriptionEdit = DescriptionEdit(self)
         self.horizontalLayout_2.addWidget(self.descriptionEdit)
         self.formLayout.setLayout(1, QtGui.QFormLayout.FieldRole, self.horizontalLayout_2)
-        self.label_3 = QtGui.QLabel(self)
+        self.label_3 = QtGui.QLabel(tr("Payee:"), self)
         self.formLayout.setWidget(2, QtGui.QFormLayout.LabelRole, self.label_3)
         self.horizontalLayout_3 = QtGui.QHBoxLayout()
         self.payeeCheckBox = QtGui.QCheckBox(self)
@@ -74,7 +77,7 @@ class MassEditionPanel(Panel):
         self.payeeEdit = PayeeEdit(self)
         self.horizontalLayout_3.addWidget(self.payeeEdit)
         self.formLayout.setLayout(2, QtGui.QFormLayout.FieldRole, self.horizontalLayout_3)
-        self.label_4 = QtGui.QLabel(self)
+        self.label_4 = QtGui.QLabel(tr("Check #"), self)
         self.formLayout.setWidget(3, QtGui.QFormLayout.LabelRole, self.label_4)
         self.horizontalLayout_4 = QtGui.QHBoxLayout()
         self.checknoCheckBox = QtGui.QCheckBox(self)
@@ -82,7 +85,7 @@ class MassEditionPanel(Panel):
         self.checknoEdit = QtGui.QLineEdit(self)
         self.horizontalLayout_4.addWidget(self.checknoEdit)
         self.formLayout.setLayout(3, QtGui.QFormLayout.FieldRole, self.horizontalLayout_4)
-        self.label_5 = QtGui.QLabel(self)
+        self.label_5 = QtGui.QLabel(tr("From:"), self)
         self.formLayout.setWidget(4, QtGui.QFormLayout.LabelRole, self.label_5)
         self.horizontalLayout_5 = QtGui.QHBoxLayout()
         self.fromCheckBox = QtGui.QCheckBox(self)
@@ -90,7 +93,7 @@ class MassEditionPanel(Panel):
         self.fromEdit = AccountEdit(self)
         self.horizontalLayout_5.addWidget(self.fromEdit)
         self.formLayout.setLayout(4, QtGui.QFormLayout.FieldRole, self.horizontalLayout_5)
-        self.label_6 = QtGui.QLabel(self)
+        self.label_6 = QtGui.QLabel(tr("To:"), self)
         self.formLayout.setWidget(5, QtGui.QFormLayout.LabelRole, self.label_6)
         self.horizontalLayout_6 = QtGui.QHBoxLayout()
         self.toCheckBox = QtGui.QCheckBox(self)
@@ -98,7 +101,7 @@ class MassEditionPanel(Panel):
         self.toEdit = AccountEdit(self)
         self.horizontalLayout_6.addWidget(self.toEdit)
         self.formLayout.setLayout(5, QtGui.QFormLayout.FieldRole, self.horizontalLayout_6)
-        self.label_7 = QtGui.QLabel(self)
+        self.label_7 = QtGui.QLabel(tr("Amount:"), self)
         self.formLayout.setWidget(6, QtGui.QFormLayout.LabelRole, self.label_7)
         self.horizontalLayout_7 = QtGui.QHBoxLayout()
         self.amountCheckBox = QtGui.QCheckBox(self)
@@ -106,7 +109,7 @@ class MassEditionPanel(Panel):
         self.amountEdit = QtGui.QLineEdit(self)
         self.horizontalLayout_7.addWidget(self.amountEdit)
         self.formLayout.setLayout(6, QtGui.QFormLayout.FieldRole, self.horizontalLayout_7)
-        self.label_8 = QtGui.QLabel(self)
+        self.label_8 = QtGui.QLabel(tr("Currency:"), self)
         self.formLayout.setWidget(7, QtGui.QFormLayout.LabelRole, self.label_8)
         self.horizontalLayout_8 = QtGui.QHBoxLayout()
         self.currencyCheckBox = QtGui.QCheckBox(self)
