d03b80c169497a93329a28837535505052c49d16
Virgil Dupras <hsoft@hardcoded.net>
2012-02-04 13:37:32 -0500

    v2.5.0

--- core/app.py
+++ core/app.py
@@ -44,7 +44,7 @@ class Application(Broadcaster, RegistrableApplication):
     
     APP_NAME = "moneyGuru"
     PROMPT_NAME = APP_NAME
-    VERSION = '2.4.3'
+    VERSION = '2.5.0'
     DEMO_LIMITATION = tr("will show this dialog on startup")
     
     def __init__(self, view, date_format='dd/MM/yyyy', decimal_sep='.', grouping_sep='', 
