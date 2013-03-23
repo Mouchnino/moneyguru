714e7b9608171394c3c1a26060dda4ed20046bdb
Virgil Dupras <hsoft@hardcoded.net>
2013-03-23 09:27:38 -0400

    Changed copyright to 2013

--- core/loader/ofx.py
+++ core/loader/ofx.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 # Created By: Eric Mc Sween
 # Created On: 2008-02-11
-# Copyright 2012 Hardcoded Software (http://www.hardcoded.net)
+# Copyright 2013 Hardcoded Software (http://www.hardcoded.net)
 # 
 # This software is licensed under the "BSD" License as described in the "LICENSE" file, 
 # which should be included with this package. The terms are also available at 
